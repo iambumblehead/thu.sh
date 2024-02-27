1256e750dd1aebed2e4526dccafde6f9649b1bec
chris <chris@bumblehead.com>
2024-02-26 17:37:16 -0800

    find way to return list from function. try shellcheck github ci

--- render-thumb-for.sh
+++ render-thumb-for.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
-echo "hello"
+scaled_wh () {
+    bgn_w=$1
+    bgn_h=$2
+    max_w=$3
+    max_h=$4
+
+    # multiply and divide by 100 to convert decimal and int
+    fin_h=$max_h
+    fin_w=$((($bgn_w * (($fin_h * 100) / $bgn_h)) / 100))
+    if [ $fin_w -ge $max_w ]; then
+        fin_w=$max_w
+        fin_h=$((($bgn_h * (($fin_w * 100) / $bgn_w)) / 100))
+    fi
+
+    echo "$fin_w $fin_h"
+}
+
+#scaled="$(scaled_wh 640 1280 200 201)"
+fin_wh=($(scaled_wh 640 1280 200 201))
+fin_w=${fin_wh[0]}
+fin_h=${fin_wh[1]}
+
+echo "$fin_w"
+echo "$fin_h"
