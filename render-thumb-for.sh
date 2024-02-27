158f43f3ae381f5563802d2dde2333b593bc14cf
chris <chris@bumblehead.com>
2024-02-26 17:46:50 -0800

    small change to pass shellcheck

--- render-thumb-for.sh
+++ render-thumb-for.sh
@@ -9,7 +9,7 @@ scaled_wh () {
     # multiply and divide by 100 to convert decimal and int
     fin_h=$max_h
     fin_w=$((($bgn_w * (($fin_h * 100) / $bgn_h)) / 100))
-    if [ $fin_w -ge $max_w ]; then
+    if [ "$fin_w" -ge "$max_w" ]; then
         fin_w=$max_w
         fin_h=$((($bgn_h * (($fin_w * 100) / $bgn_w)) / 100))
     fi
@@ -18,7 +18,8 @@ scaled_wh () {
 }
 
 #scaled="$(scaled_wh 640 1280 200 201)"
-fin_wh=($(scaled_wh 640 1280 200 201))
+#fin_wh=($(scaled_wh 640 1280 200 201))
+fin_wh=($(scaled_wh 640 480 200 201))
 fin_w=${fin_wh[0]}
 fin_h=${fin_wh[1]}
 
