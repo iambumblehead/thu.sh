bc667934594877bc3b6e119b4f40f8bcdee3ae42
chris <chris@bumblehead.com>
2024-03-28 13:10:31 -0700

    replace test pdf asset with more common dimension

--- test/test_unit.sh
+++ test/test_unit.sh
@@ -90,7 +90,7 @@ test_zip_move_file_out () {
 }
 
 test_thumb_create_from_pdf_magcick () {
-    pdfpath_inpdf="./asset/test.pdf"
+    pdfpath_inpdf="./asset/test.640x480.pdf"
     pdfpath_outimg=$(thumb_create_from_pdf_magick "$pdfpath_inpdf" "400x400")
 
     assert_matches "pdf.400x400.jpg$" "$pdfpath_outimg" \
